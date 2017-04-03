26e90187fc0960207c2752d8757386ba35ea877d
Mike Moore <mike@themsquared.com>
2017-04-03 15:02:39 -0400

    Config Checking

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var config = require('./config');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 var read = require('read-data');
 var config = read.sync('config.yaml');
 
+console.log(config);
+
 // Do something fanicer here, but for now pass the object
 module.exports = config
